9cd0c59694a7d2d31199be81063233b25fefbe5d
Ralph Schaer <ralphschaer@gmail.com>
2013-12-13 09:24:21 +0100

    Use the extraced model generator

--- src/main/java/ch/rasc/edsutil/entity/AbstractPersistable.java
+++ src/main/java/ch/rasc/edsutil/entity/AbstractPersistable.java
@@ -5,7 +5,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
-import ch.ralscha.extdirectspring.generator.ModelField;
+import ch.rasc.extclassgenerator.ModelField;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
